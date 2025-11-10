ee365efe1fd5fc768e860234becbc3da07651a7d
MattRiddick <mriddick5279@gmail.com>
2025-11-10 01:49:26 -0500

    Created about page

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+    return(
+        <main>
+            <h1 style={{textAlign: 'center', fontWeight: 'bold', fontSize: '3rem'}}>About the Developers</h1>
+        </main>
+    );
+}
