ebfd391493816e2116ec7c0699e723d8c577eacf
mriddick5279 <mriddick5279@gmail.com>
2025-11-13 11:21:56 -0500

    Removing commented out code not needed anymore

--- components/supabaseSearch.tsx
+++ components/supabaseSearch.tsx
@@ -4,10 +4,6 @@ import { createClient } from '@/utils/supabase/server';
 
 export async function supabaseSearch(name: string) {
     const supabase = await createClient();
-    // const { data: names, error } = await supabase
-    //     .from('company')
-    //     .select('name')
-    //     .ilike('name', `%${name}%`);
 
     const {data: comptags, error: err} = await supabase
         .from('company')
